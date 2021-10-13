34da48bcd43cc39761ac02cbeb208c6a7c3dac80
AishSunbae <gdori2018@gmail.com>
2021-10-13 22:16:19 +0200

    add icons,  add theme switcher
    added icons to the skills

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,15 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// theme switcher functionality
+const input = document.querySelector('.theme-switcher input');
+
+input.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    document.body.setAttribute('data-theme', 'dark');
+  }
+  else {
+    document.body.setAttribute('data-theme', 'light');
+  }
+})
